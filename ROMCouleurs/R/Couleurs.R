c60dbe2067edc9972ce141077d65441ffbf46cb5
ProjetsROM <162568882+ProjetsROM@users.noreply.github.com>
2024-06-24 09:27:41 +0200

    ROMCouleurs V1

---
+++ ROMCouleurs/R/Couleurs.R
@@ -0,0 +1,68 @@
+#' Couleur des <span style="color:rgb(0,112,213);">Narrow Body</span> pour le LTMF
+#' @export
+ROM_Coul_NB <- rgb(0, 112, 213, maxColorValue = 255)
+#' Couleur des <span style="color:rgb(112, 48, 160);">Wide Body</span> pour le LTMF
+#' @export
+ROM_Coul_WB <- rgb(112, 48, 160, maxColorValue = 255)
+#' Couleur des <span style="color:rgb(0, 176, 80);">Regional Jet</span> pour le LTMF
+#' @export
+ROM_Coul_RJ <- rgb(0, 176, 80, maxColorValue = 255)
+#' Couleur des <span style="color:rgb(255, 152, 0);">Turboreacteurs</span> pour le LTMF
+#' @export
+ROM_Coul_RT <- rgb(255, 152, 0, maxColorValue = 255)
+
+#' Couleur de la region <span style="color:rgb(242, 139, 91);">Afrique Moyen Orient</span> pour le LTMF
+#' @export
+ROM_Coul_AFRMY <- rgb(242, 139, 91, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(188, 214, 241);">Amerique Centrale et Amerique du Sud</span> pour le LTMF
+#' @export
+ROM_Coul_AMCS <- rgb(188, 214, 241, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(43, 177, 236);">Amerique du Nord</span> pour le LTMF
+#' @export
+ROM_Coul_AMN <- rgb(43, 177, 236, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(204, 186, 220);">Asie Australie</span> pour le LTMF
+#' @export
+ROM_Coul_ASIAUS <- rgb(204, 186, 220, maxColorValue = 255)
+#' Couleur de la region de <span style="color:rgb(173, 204, 83);">CEI (CIS en anglais)</span> pour le LTMF
+#' @export
+ROM_Coul_CEI <- rgb(173, 204, 83, maxColorValue = 255)
+#' Couleur de la <span style="color:rgb(158, 117, 178);">Chine</span> pour le LTMF
+#' @export
+ROM_Coul_CHN <- rgb(158, 117, 178, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(149, 193, 31);">Europe</span> pour le LTMF
+#' @export
+ROM_Coul_EUR <- rgb(149, 193, 31, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(136, 84, 157);">Inde</span> pour le LTMF
+#' @export
+ROM_Coul_SIN <- rgb(136, 84, 157, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(134, 196, 236);">Amerique Centrale</span> pour le LTMF
+#' @export
+ROM_Coul_AMC <- rgb(134, 196, 236, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(188, 214, 241);">Amerique du Sud</span> pour le LTMF
+#' @export
+ROM_Coul_AMS <- rgb(188, 214, 241, maxColorValue = 255)
+#' Couleur des regions <span style="color:rgb(149, 193, 31);">Europe et de la CEI</span> pour le LTMF
+#' @export
+ROM_Coul_EURC <- rgb(149, 193, 31, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(233, 78, 27);">Afrique du Nord</span> pour le LTMF
+#' @export
+ROM_Coul_AFN <- rgb(233, 78, 27, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(238, 112, 60);">Afrique Centrale</span> pour le LTMF
+#' @export
+ROM_Coul_AFC <- rgb(238, 112, 60, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(242, 139, 91);">Afrique du Sud</span> pour le LTMF
+#' @export
+ROM_Coul_AFS <- rgb(242, 139, 91, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(115, 53, 140);">Moyen Orient</span> pour le LTMF
+#' @export
+ROM_Coul_MY <- rgb(115, 53, 140, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(180, 151, 199);">Asie du Nord Est</span> pour le LTMF
+#' @export
+ROM_Coul_ASN <- rgb(180, 151, 199, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(204, 186, 220);">Asie du Sud Est</span> pour le LTMF
+#' @export
+ROM_Coul_ASE <- rgb(204, 186, 220, maxColorValue = 255)
+#' Couleur de la region <span style="color:rgb(237, 107, 149);">Australie</span> pour le LTMF
+#' @export
+ROM_Coul_AUS <- rgb(237, 107, 149, maxColorValue = 255)
+
